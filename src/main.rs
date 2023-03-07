67415bd21c73215d3e9f2de6faecf080ad8f552d
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-07 17:01:19 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -13,4 +13,13 @@ fn main() {
     let device = host
         .default_input_device()
         .expect("Failed to get default input device");
+
+    // Get the format of the default input device
+    let config = device.default_input_config().unwrap();
+
+    // Create a vector to store the captured audio data
+    let mut audio_data = Vec::new();
+
+    // Create a channel for the push-to-talk input
+    let (tx, rx): (std::sync::mpsc::Sender<bool>, Receiver<bool>) = channel();
 }
