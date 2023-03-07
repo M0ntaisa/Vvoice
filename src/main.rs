1bcd0a454bb95d6721deee2de851bf797dade885
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-03-07 17:02:09 +0800

    -

--- src/main.rs
+++ src/main.rs
@@ -22,4 +22,19 @@ fn main() {
 
     // Create a channel for the push-to-talk input
     let (tx, rx): (std::sync::mpsc::Sender<bool>, Receiver<bool>) = channel();
+
+    // Create a callback that writes the input stream to the audio_data vector
+    let callback = move |data: &[i16], _: &cpal::InputCallbackInfo| {
+        if let Ok(push_to_talk) = rx.try_recv() {
+            if push_to_talk {
+                audio_data.extend_from_slice(data);
+            }
+        }
+    };
+
+    // Start the audio stream and run the event loop
+    let stream_id = event_loop
+        .build_input_stream(&device, &config.into(), callback, ())
+        .unwrap();
+    event_loop.play_stream(stream_id).unwrap();
 }
